ce94d796c19559f2f660ce985f5da7daf8eef6b0
Ryan McGovern <ryan.mcgovern@jestercom.com>
2016-04-21 18:22:10 -0400

    command testing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var tmi = require('tmi.js');
+var streamer = "Capitaahh";
 
 var options = {
 	options: {
@@ -12,12 +13,36 @@ var options = {
 		username: "CapitaahhBot",
 		password: "oauth:ztkttj2rewy7s3nt15gleunq2cfx6z"
 	},
-	channels: ["Capitaahh"]
+	channels: [streamer]
 };
 
 var client = new tmi.client(options);
 client.connect();
 
 client.on('connected', function(address, port) {
-	client.action("Capitaahh", "aaaaaahhhhhhh");
+	client.action(streamer, "aaaaaahhhhhhh");
 });
+
+client.on("chat", function(channel, user, message, self) {
+	if (!self) {
+		var split = message.toLowerCase().split(" ");
+
+		switch (split[0]) {
+			case "!mods":
+				client.mods("channel").then(function(data) {
+					for (var i = 0; i <= data.length; i++) {
+						var data = user["display-name"] || user["username"];
+				   		 client.action(streamer, data);
+					};
+				    
+				}).catch(function(err) {
+				   
+				});
+				client.action(streamer, "modlist");
+				break;
+			case "!bot":
+				client.action(streamer, "beep boop");
+				break;
+		}
+	}
+})
